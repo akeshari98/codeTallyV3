b0fbf318c4271b80219ec18cecdd7e7d31601649
akeshari986 <22bcs055@iiitdmj.ac.in>
2024-08-11 10:48:00 +0530

    three

--- server/model/redisModel.js
+++ server/model/redisModel.js
@@ -1,7 +1,10 @@
 const redis = require('redis');
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+    host: '13.61.4.166',
+    port: 6379, // Default Redis port
+});
 
-redisClient.on('ready', async function() {
+redisClient.on('ready', function() {
     console.log('Connected to Redis');
 });
 
